3a91a296e5451049a84e2e04e8f1b9964866520e
Ahmad <167347571+M-AhmadSuhail@users.noreply.github.com>
2024-12-14 18:52:04 +0800

    Add files via upload
    updated controller

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -26,7 +26,7 @@ import javax.swing.*;
  */
 public class Controller implements Observer {
 
-	public static final String LEVEL_ONE_CLASS_NAME = "com.example.demo.Levels.LevelBoss";
+	public static final String LEVEL_ONE_CLASS_NAME = "com.example.demo.Levels.LevelOne";
 
 	private final Stage stage;
 	private final LevelView levelView;
